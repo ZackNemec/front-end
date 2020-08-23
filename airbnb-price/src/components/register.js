1474818ac7006733a7981184385b8f40c256c3b2
ZackNemec <zacknemec@live.com>
2020-08-23 09:03:19 -0700

    this push covers updated register and slight addition to navigation

--- airbnb-price/src/components/register.js
+++ airbnb-price/src/components/register.js
@@ -4,6 +4,20 @@ import * as yup from "yup";
 import axios from "axios";
 
 const Register = () => {
+  const [regData, setRegData] = React.useState({
+    username: "",
+    password: "",
+  });
+  const [confirm, setConfirm] = React.useState({
+    confirmPassword: "",
+  });
+  const [errors, setErrors] = React.useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [serverError, setServerError] = React.useState("");
+
   const formSchema = yup.object().shape({
     username: yup
       .string()
@@ -14,42 +28,65 @@ const Register = () => {
       .string()
       .required("Password is required")
       .min(8, "Password is too short, 8 characters minimum")
-      .matches(/[a-zA-Z]/, "Password can only contain Latin letters"),
-    passwordConfirm: yup
+      .matches(/[a-zA-Z0-9]/, "can only use Latin Letters and Numerals"),
+    confirmPassword: yup
       .string()
+      .required("Passwords must match")
       .oneOf([yup.ref("password"), null], "Passwords must match"),
   });
-  const [errors, setErrors] = React.useState({
-    username: "",
-    password: "",
-  });
-
-  const [regData, setRegData] = React.useState({
-    username: "",
-    password: "",
-  });
-  const changeHandler = (e) => {
-    setRegData({ ...regData, [e.target.name]: e.target.value });
-  };
-  const submit = () => {
-    axios
-      .post("https://buildweek-airbnb.herokuapp.com/api/auth/register", regData)
-      .then((results) => {
-        console.log("returned data from post", results.data);
+  const validateChange = (e) => {
+    yup
+      .reach(formSchema, e.target.name)
+      .validate(e.target.value)
+      .then((valid) => {
+        setErrors({ ...errors, [e.target.name]: "" });
       })
-      .catch((error) => {
-        console.log(error);
+      .catch((err) => {
+        console.log(err);
+        setErrors({ ...errors, [e.target.name]: err.errors[0] });
       });
   };
+
+  const changeHandler = (e) => {
+    setRegData({ ...regData, [e.target.name]: e.target.value });
+    e.persist();
+    console.log(e.target.name, e.target.value);
+    validateChange(e);
+    console.log(errors);
+  };
+  const confirmHandler = (e) => {
+    setConfirm({ ...confirm, [e.target.name]: e.target.value });
+    e.persist();
+    console.log("input changed", e.target.value);
+    validateChange(e);
+  };
+
+  const submit = (e) => {
+    formSchema.validate(regData, confirm).then(() => {
+      axios
+        .post(
+          "https://buildweek-airbnb.herokuapp.com/api/auth/register",
+          regData
+        )
+        .then((results) => {
+          console.log("returned data from post", results.data);
+        })
+        .catch((err) => {
+          setServerError(`Username is take, please try ${regData.username}1`);
+        });
+    });
+  };
+
   return (
     <>
       <Form
-        onSubmit={(event) => {
-          event.preventDefault();
+        onSubmit={(e) => {
+          e.preventDefault();
           submit();
           console.log(regData);
         }}
       >
+        {serverError ? <p className="error">{serverError}</p> : null}
         <FormGroup>
           <Label for="usernameInput">UserName</Label>
           <Input
@@ -60,7 +97,7 @@ const Register = () => {
             value={regData.username}
             onChange={changeHandler}
           />
-          {errors.username.length < 4 ? (
+          {errors.username.length > 3 || errors.username.length < 15 ? (
             <p className="error">{errors.username}</p>
           ) : null}
         </FormGroup>
@@ -74,6 +111,23 @@ const Register = () => {
             value={regData.password}
             onChange={changeHandler}
           />
+          {errors.password.length > 7 ? (
+            <p className="error">{errors.password}</p>
+          ) : null}
+        </FormGroup>
+        <FormGroup>
+          <Label for="confirmPasswordInput">Confirm Password</Label>
+          <Input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            id="confirmPasswordInput"
+            value={confirm.confirmPassword}
+            onChange={confirmHandler}
+          />
+          {errors.confirmPassword === regData.password ? null : (
+            <p className="error">{errors.confirmPassword}</p>
+          )}
         </FormGroup>
         <Button>Submit</Button>
       </Form>
